8b920c8fb5aa08c704ae88e225bb3cc9a8cdf2e1
Yaroslav <loghitechdf@gmail.com>
2024-08-10 12:59:15 +0300

    Initial commit

---
+++ src/main/java/com/application/bsuserservice/BsUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.application.bsuserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BsUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BsUserServiceApplication.class, args);
+    }
+
+}
