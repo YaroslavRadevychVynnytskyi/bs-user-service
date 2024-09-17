31a60a9d497efff43734aec3755e148a52815cb8
Yaroslav Radevych-Vynnytskyi <124635285+YaroslavRadevychVynnytskyi@users.noreply.github.com>
2024-09-17 17:30:00 +0300

    Introduced JWT Auth (#1)
    * Introduced JWT Auth
    
    * Introduced JWT Auth

--- src/main/java/com/application/bsuserservice/BsUserServiceApplication.java
+++ src/main/java/com/application/bsuserservice/BsUserServiceApplication.java
@@ -2,8 +2,12 @@ package com.application.bsuserservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
 public class BsUserServiceApplication {
 
     public static void main(String[] args) {
